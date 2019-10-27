f09cf64e349951d519d4fc9b626dfa4ecf0cf60c
Chun Shen <chunshen1987@gmail.com>
2019-10-27 13:31:20 -0400

    get correct number of cores for MacOS

--- external_codes/get_smash.sh
+++ external_codes/get_smash.sh
@@ -5,8 +5,14 @@ git clone --depth=1 https://github.com/smash-transport/smash.git
 
 # 2) Compile SMASH
 cd smash && mkdir build && cd build
-cmake .. # -DPythia_CONFIG_EXECUTABLE=${PYTHIA8DIR}/bin/pythia8-config
-number_of_cores=`nproc --all`
+cmake .. -DPythia_CONFIG_EXECUTABLE=${PYTHIA8DIR}/bin/pythia8-config
+
+machine="$(uname -s)"
+case "${machine}" in
+    Linux*)     number_of_cores=`nproc --all`;;
+    Darwin*)    number_of_cores=`sysctl -n hw.ncpu`;;
+    *)          number_of_cores=1;;
+esac
 number_of_cores_to_compile=$(( ${number_of_cores} > 20 ? 20 : ${number_of_cores} ))
 echo "Compiling SMASH using ${number_of_cores_to_compile} cores."
 make -j${number_of_cores} smash smash_shared
