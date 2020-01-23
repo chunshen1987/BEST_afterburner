1e15dee11e981c70df82c96920044b156f8262b1
Dmytro Oliinychenko <doliinychenko@lbl.gov>
2020-01-23 22:39:38 +0100

    Auto download of smash analysis scripts
    Signed-off-by: Dmytro Oliinychenko <doliinychenko@lbl.gov>

--- external_codes/get_smash.sh
+++ external_codes/get_smash.sh
@@ -16,3 +16,6 @@ esac
 number_of_cores_to_compile=$(( ${number_of_cores} > 20 ? 20 : ${number_of_cores} ))
 echo "Compiling SMASH using ${number_of_cores_to_compile} cores."
 make -j${number_of_cores} smash smash_shared
+
+# 3) Get smash analysis scripts
+git clone --depth=1 https://github.com/smash-transport/smash-analysis.git
