692d46100bf40c4dce4384e93b6b102b51038393
Dmytro Oliinychenko <doliinychenko@lbl.gov>
2020-01-30 21:53:28 +0100

    Compile pratt sampler automatically
    Signed-off-by: Dmytro Oliinychenko <doliinychenko@lbl.gov>

--- external_codes/get_best_sampler.sh
+++ external_codes/get_best_sampler.sh
@@ -4,6 +4,5 @@
 git clone --depth=1 https://github.com/steinhor/best_sampler.git
 
 # 2) Compile
-# Todo: to be updated, when the best sampler is ready
-# cd best_sampler && mkdir build && cd build
-# cmake .. && make -j
+cd best_sampler && mkdir build && cd build
+cmake .. && make -j
