5654766dc0d82f7ac7fb5cf69d8bf7929cecf408
Dmytro Oliinychenko <doliinychenko@lbl.gov>
2020-09-03 01:47:49 +0200

    Get pythia automatically
    Signed-off-by: Dmytro Oliinychenko <doliinychenko@lbl.gov>

--- external_codes/get_smash.sh
+++ external_codes/get_smash.sh
@@ -3,9 +3,19 @@
 # 1) Download the SMASH code
 git clone --depth=1 https://github.com/smash-transport/smash.git
 
-# 2) Compile SMASH
+# 2) Get the right version of Pythia right here (newer SMASH may need a different version)
+(
+wget http://home.thep.lu.se/~torbjorn/pythia8/pythia8235.tgz
+tar xf pythia8235.tgz && rm pythia8235.tgz
+cd pythia8235
+./configure --cxx-common='-std=c++11 -march=native -mfpmath=sse -O3 -fPIC'
+make
+)
+
+# 3) Compile SMASH
+(
 cd smash && mkdir build && cd build
-cmake .. -DPythia_CONFIG_EXECUTABLE=${PYTHIA8DIR}/bin/pythia8-config
+cmake .. -DPythia_CONFIG_EXECUTABLE=../../pythia8235/bin/pythia8-config
 
 machine="$(uname -s)"
 case "${machine}" in
@@ -16,6 +26,7 @@ esac
 number_of_cores_to_compile=$(( ${number_of_cores} > 20 ? 20 : ${number_of_cores} ))
 echo "Compiling SMASH using ${number_of_cores_to_compile} cores."
 make -j${number_of_cores} smash smash_shared
+)
 
-# 3) Get smash analysis scripts
+# 4) Get smash analysis scripts
 git clone --depth=100 https://github.com/smash-transport/smash-analysis.git
