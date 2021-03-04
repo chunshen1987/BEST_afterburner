9cc3ffe1ee0f7d72dd67fb61b2be90bc80ba122b
Dmytro Oliinychenko <doliinychenko@lbl.gov>
2021-03-04 08:12:07 +0100

    Update to newest SMASH version
    Signed-off-by: Dmytro Oliinychenko <doliinychenko@lbl.gov>

--- external_codes/get_smash.sh
+++ external_codes/get_smash.sh
@@ -5,17 +5,17 @@ git clone --depth=1 https://github.com/smash-transport/smash.git
 
 # 2) Get the right version of Pythia right here (newer SMASH may need a different version)
 (
-wget http://home.thep.lu.se/~torbjorn/pythia8/pythia8235.tgz
-tar xf pythia8235.tgz && rm pythia8235.tgz
-cd pythia8235
-./configure --cxx-common='-std=c++11 -march=native -mfpmath=sse -O3 -fPIC'
-make
+  wget http://home.thep.lu.se/~torbjorn/pythia8/pythia8303.tgz
+  tar xf pythia8303.tgz && rm pythia8303.tgz
+  cd pythia8303
+  ./configure --cxx-common='-std=c++11 -march=native -mfpmath=sse -O3 -fPIC'
+  make
 )
 
 # 3) Compile SMASH
 (
 cd smash && mkdir build && cd build
-cmake .. -DPythia_CONFIG_EXECUTABLE=../../pythia8235/bin/pythia8-config -DUSE_ROOT=OFF
+cmake .. -DPythia_CONFIG_EXECUTABLE=../../pythia8303/bin/pythia8-config -DUSE_ROOT=OFF
 
 machine="$(uname -s)"
 case "${machine}" in
