b752dd2d60b9d8b9c09a1db02a8d47002bfa619c
Chun Shen <chunshen1987@gmail.com>
2022-11-16 21:23:46 -0500

    fixed a typo

--- external_codes/get_microcanonical_sampler.sh
+++ external_codes/get_microcanonical_sampler.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[ -d "microcanonical_cooper_frye"] && rm -fr microcanonical_cooper_frye
+[ -d "microcanonical_cooper_frye" ] && rm -fr microcanonical_cooper_frye
 
 git clone --depth=1 https://github.com/chunshen1987/microcanonical_cooper_frye
 
