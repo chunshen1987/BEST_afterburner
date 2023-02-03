f237564d4a551513894e584a17c16cae5bfd04a9
Chun Shen <chunshen1987@gmail.com>
2023-02-03 16:59:30 -0500

    update on the get_smash script

--- external_codes/get_smash.sh
+++ external_codes/get_smash.sh
@@ -2,8 +2,16 @@
 
 [ -d "smash" ] && rm -fr smash
 
+machine="$(uname -s)"
+case "${machine}" in
+    Linux*)     number_of_cores=`nproc --all`;;
+    Darwin*)    number_of_cores=`sysctl -n hw.ncpu`;;
+    *)          number_of_cores=1;;
+esac
+number_of_cores_to_compile=$(( ${number_of_cores} > 10 ? 10 : ${number_of_cores} ))
+
 # 1) Download the SMASH code
-git clone --depth=1 https://github.com/smash-transport/smash.git
+git clone -b SMASH-2.2.1 --depth=1 https://github.com/smash-transport/smash.git
 
 # 2) Get the right version of Pythia right here (newer SMASH may need a different version)
 (
@@ -11,7 +19,9 @@ git clone --depth=1 https://github.com/smash-transport/smash.git
     tar xf pythia8307.tar.bz2 && rm pythia8307.tar.bz2
     cd pythia8307
     ./configure --cxx-common='-std=c++11 -O3 -fPIC'
-    make
+
+    echo "Compiling PYTHIA using ${number_of_cores_to_compile} cores."
+    make -j${number_of_cores_to_compile}
 )
 
 # 3) Compile SMASH
@@ -21,16 +31,9 @@ git clone --depth=1 https://github.com/smash-transport/smash.git
     mkdir -p build && cd build
     cmake .. -DPythia_CONFIG_EXECUTABLE=../../pythia8307/bin/pythia8-config -DUSE_ROOT=OFF
 
-    machine="$(uname -s)"
-    case "${machine}" in
-        Linux*)     number_of_cores=`nproc --all`;;
-        Darwin*)    number_of_cores=`sysctl -n hw.ncpu`;;
-        *)          number_of_cores=1;;
-    esac
-    number_of_cores_to_compile=$(( ${number_of_cores} > 10 ? 10 : ${number_of_cores} ))
     echo "Compiling SMASH using ${number_of_cores_to_compile} cores."
     make -j${number_of_cores} smash smash_shared
 )
 
 # 4) Get smash analysis scripts
-git clone --depth=1 https://github.com/smash-transport/smash-analysis.git
+#git clone --depth=1 https://github.com/smash-transport/smash-analysis.git
