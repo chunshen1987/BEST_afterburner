ff50143362ee772add7b5518c774265ef56eed00
Chun Shen <chunshen@wayne.edu>
2025-06-06 12:27:55 -0400

    update the iSS version

--- external_codes/get_iSS.sh
+++ external_codes/get_iSS.sh
@@ -3,4 +3,4 @@
 [ -d "iSS" ] && rm -fr iSS
 
 # download the code package
-git clone --depth=1 https://github.com/chunshen1987/iSS iSS
+git clone --depth=1 https://github.com/chunshen1987/iSS -b dev iSS
