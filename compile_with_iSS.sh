41cd0ca199a1dbaf4cdf917aac7edf275d127c34
Chun <chunshen@wayne.edu>
2026-01-03 15:35:06 -0500

    update the SMASH version to 3.3

--- compile_with_iSS.sh
+++ compile_with_iSS.sh
@@ -6,7 +6,7 @@ case "${machine}" in
     Darwin*)    number_of_cores=`sysctl -n hw.ncpu`;;
     *)          number_of_cores=1;;
 esac
-number_of_cores_to_compile=$(( ${number_of_cores} > 10 ? 10 : ${number_of_cores} ))
+number_of_cores_to_compile=$(( ${number_of_cores} > 4 ? 4 : ${number_of_cores} ))
 
 (
     cd external_codes
--- external_codes/get_smash.sh
+++ external_codes/get_smash.sh
@@ -8,16 +8,16 @@ case "${machine}" in
     Darwin*)    number_of_cores=`sysctl -n hw.ncpu`;;
     *)          number_of_cores=1;;
 esac
-number_of_cores_to_compile=$(( ${number_of_cores} > 10 ? 10 : ${number_of_cores} ))
+number_of_cores_to_compile=$(( ${number_of_cores} > 4 ? 4 : ${number_of_cores} ))
 
 # 1) Download the SMASH code
-git clone -b SMASH-2.2.1 --depth=1 https://github.com/smash-transport/smash.git
+git clone -b SMASH-3.3 --depth=1 https://github.com/smash-transport/smash.git
 
 # 2) Get the right version of Pythia right here (newer SMASH may need a different version)
 (
-    wget https://pythia.org/download/pythia83/pythia8307.tar.bz2
-    tar xf pythia8307.tar.bz2 && rm pythia8307.tar.bz2
-    cd pythia8307
+    wget https://pythia.org/download/pythia83/pythia8316.tar.bz2
+    tar xf pythia8316.tar.bz2 && rm pythia8316.tar.bz2
+    cd pythia8316
     ./configure --cxx-common='-std=c++11 -O3 -fPIC'
 
     echo "Compiling PYTHIA using ${number_of_cores_to_compile} cores."
@@ -29,7 +29,7 @@ git clone -b SMASH-2.2.1 --depth=1 https://github.com/smash-transport/smash.git
     cd smash
     rm cmake/FindGSL.cmake
     mkdir -p build && cd build
-    cmake .. -DPythia_CONFIG_EXECUTABLE=../../pythia8307/bin/pythia8-config -DUSE_ROOT=OFF
+    cmake .. -DPythia_CONFIG_EXECUTABLE=../../pythia8316/bin/pythia8-config -DUSE_ROOT=OFF
 
     echo "Compiling SMASH using ${number_of_cores_to_compile} cores."
     make -j${number_of_cores} smash smash_shared
